0c18093a6f06aff79b5bae304491d05ac5c80976
horacio_skrp <hervekudayah@gmail.com>
2024-06-05 17:34:37 +0000

    ✨ initial commit

---
+++ src/main/java/com/horacioskrp/blogio/BlogioApplication.java
@@ -0,0 +1,13 @@
+package com.horacioskrp.blogio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/horacioskrp/blogio/BlogioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horacioskrp.blogio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
